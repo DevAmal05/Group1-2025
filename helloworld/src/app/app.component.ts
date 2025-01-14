81ef6cfbc9eafcd4d42aac3da028afc4938209c5
DevAmal05 <62183621+DevAmal05@users.noreply.github.com>
2025-01-14 19:49:57 +0200

    From Ts to Html

--- helloworld/src/app/app.component.ts
+++ helloworld/src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
+
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'helloworld';
+
+
+title = "Angular";
+condition=false;
+num=123;
+
+source="assets/angular.png";
+
+nameList= ['Ahmed', 'Ali', 'Imen'];
+
+
 }
